e8e38cb1669756a7f65495b725994893a15ebfec
stefan <stefanstojanovicbusiness73@gmail.com>
2021-01-06 01:34:45 +0100

    viznjerova gotova

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,22 @@
 const cesarCipher = () => {
     let otvoreniTekst = document.querySelector(".main-input").value,
         brojPomeranja = parseInt(document.querySelector(".shift").value),
-        alphabet = "abcdefghijklmnopqrstuvwxyz0123456789",
+        alphabet = "abcdefghijklmnopqrstuvwxyz",
         sifrat = "", helper, newIndex;
-
     otvoreniTekst.split("").forEach((letter) => {
         newIndex = alphabet.indexOf(letter.toLowerCase());
         // if its -1 then its a sign or a space
         if (newIndex !== -1) {
             newIndex += brojPomeranja;
-            if (newIndex > alphabet.length) {
+            if (newIndex >= alphabet.length) {
                 newIndex = newIndex % alphabet.length;
+                console.log(newIndex);
             }
             if (letter.toLowerCase() !== letter) {
                 sifrat += alphabet[newIndex].toUpperCase();
             }
             else {
-                sifrat+= alphabet[newIndex];
+                sifrat += alphabet[newIndex];
             }
         }
         else {
@@ -28,10 +28,33 @@ const cesarCipher = () => {
     document.querySelector(".main-input").value = sifrat;
 }
 
+const vignerCipher = () => {
+    let otvoreniTekst = document.querySelector(".main-input").value.split(" ").join(""),
+        kljuc = document.getElementsByName("kljuc")[0].value.split(" ").join(""),
+        alphabet = "abcdefghijklmnopqrstuvwxyz",
+        sifrat = "", helper, newIndex;
+
+    let kljucDuzina = kljuc.length;
+
+    otvoreniTekst.split("").forEach((letter, idx) => {
+        newIndex = alphabet.indexOf(letter.toLowerCase()) + alphabet.indexOf(kljuc[idx % kljucDuzina].toLowerCase());
+        if ( newIndex >= 26 ){
+            newIndex %= 26;
+        }
+        if (letter.toLowerCase() !== letter){
+            sifrat+=alphabet[newIndex].toUpperCase();
+        }
+        else{
+            sifrat+=alphabet[newIndex];
+        }
+    });
+    document.querySelector(".main-input").value = sifrat;
+}
+
 const generateOptions = () => {
     let root = document.getElementsByClassName("shift")[0],
         option;
-    Array.from(new Array(36)).forEach((_, idx) => {
+    Array.from(new Array(26)).forEach((_, idx) => {
         if (idx !== 0) {
             option = document.createElement("option");
             option.value = idx;
