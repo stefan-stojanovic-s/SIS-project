726f056ccff2d87b4e9bee8a0e947a76674a3862
stefan <stefanstojanovicbusiness73@gmail.com>
2021-01-09 02:58:08 +0100

    started playfair finished afina

--- scripts/index.js
+++ scripts/index.js
@@ -38,17 +38,89 @@ const vignerCipher = () => {
 
     otvoreniTekst.split("").forEach((letter, idx) => {
         newIndex = alphabet.indexOf(letter.toLowerCase()) + alphabet.indexOf(kljuc[idx % kljucDuzina].toLowerCase());
-        if ( newIndex >= 26 ){
+        if (newIndex >= 26) {
             newIndex %= 26;
         }
-        if (letter.toLowerCase() !== letter){
-            sifrat+=alphabet[newIndex].toUpperCase();
+        if (letter.toLowerCase() !== letter) {
+            sifrat += alphabet[newIndex].toUpperCase();
         }
-        else{
-            sifrat+=alphabet[newIndex];
+        else {
+            sifrat += alphabet[newIndex];
         }
     });
     document.querySelector(".main-input").value = sifrat;
+}
+
+const afinaCipher = () => {
+    let otvoreniTekst = document.querySelector(".main-input").value.split(" ").join(""),
+        a = parseInt(document.getElementsByName("kljuc-a")[0].value.split(" ").join("")),
+        b = parseInt(document.getElementsByName("kljuc-b")[0].value.split(" ").join("")),
+        alphabet = "abcdefghijklmnopqrstuvwxyz",
+        sifrat = "", helper, newIndex;
+
+
+    otvoreniTekst.split("").forEach((letter, idx) => {
+        newIndex = (alphabet.indexOf(letter.toLowerCase()) * a + b) % 26;
+        sifrat += alphabet[newIndex];
+    });
+    document.querySelector(".main-input").value = sifrat;
+}
+
+const playfairCipher = () => {
+
+    const getFirstNonRepeatedLetter = (alphabet , text) => {
+        let newLetter = "";
+        alphabet = alphabet.replace("i", "j");
+        for(let i = 0; i < alphabet.length; i++){
+            if ( text.indexOf(alphabet[i]) < 0 ){
+                newLetter = alphabet[i];
+                break;
+            }
+        }
+        return newLetter
+    }
+
+    let otvoreniTekst = document.querySelector(".main-input").value.split(" ").join(""),
+        alphabet = "abcdefghijklmnopqrstuvwxyz",
+        sifrat = "", helper, newIndex;
+
+    let matrix = new Array(5).fill(new Array(5).fill());
+    let matrixNew = [[], [], [], [], []];
+
+    let newOt = "";
+
+
+    var counter = 0;
+    let temp = [];
+    let toPush;
+
+    //get rid of dupes
+    // i = j;
+    otvoreniTekst.toLowerCase().split("").forEach((letter) => {
+        if (letter === "i") {
+            letter = "j";
+        }
+        if (newOt.indexOf(letter) < 0) {
+            newOt += letter;
+        }
+    })
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (counter <= newOt.length - 1) {
+                toPush = newOt[counter].toLowerCase();
+                matrixNew[i].push(toPush);
+            }
+            else{
+                let newLetter = getFirstNonRepeatedLetter(alphabet,newOt);
+                newOt+=newLetter;
+                matrixNew[i].push(newLetter);
+            }
+            counter = counter + 1;
+        }
+    }
+
+    console.log(matrixNew);
 }
 
 const generateOptions = () => {
